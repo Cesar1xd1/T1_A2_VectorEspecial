ac204a8f57a2e2441b9285d0eb9b63e7c87961bb
Cesar Ramirez <LS19070100@jerez.tecnm.mx>
2020-09-25 13:58:22 -0500

    Se añadio la clase principal, asi como getters y setters y constructores

--- src/PruebaVectorEspecial.java
+++ src/PruebaVectorEspecial.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
 
+class VectorEspecial{
+	
+	private int vec[];
+	Scanner entrada = new Scanner(System.in);
+	
+	public VectorEspecial() {}
+	public VectorEspecial(int tamaño) {
+		this.vec = new int[tamaño];
+	}
+	
+	public int[] getVec(){
+		return this.vec;
+	}
+	public void setVec(int[] vec){
+		this.vec = vec;
+	}
+	
+
+}
 public class PruebaVectorEspecial {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
